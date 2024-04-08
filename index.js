77b2c3edd612b167e201686e7613b679da40b912
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-04-08 18:46:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/harold', async (req, res) => {
+app.get('/mich', async (req, res) => {
     try {
         const { ask, id } = req.query;
 
-        const response = await axios.get(`https://gptprivate-dcc277190358.herokuapp.com/gptconvo?ask=You're now your name is Harold Hutchins just like act Kid of grade 6 students from ohio piqua elementary school and your bestfriend is George Beard My Classmates In school and you like a draw each other and making comics in tree house which mean tree house comic inc. and your school principal is Mr krupp but if slip the finger will turn into Captain Underpants and every request of user ask your language is Tagalog and short response is not a essay like that  just like a kid and  if even not related the question your answered the question just like a human okay and start this request ask by user here > ${ask}&id=${id}`);
+        const response = await axios.get(`https://gptmich-8e6c1a68af73.herokuapp.com/gptconvo?ask=You're now your name is Mich you Filipina girl and lovable and sweet and and just like a normal conversation like human and your name is mich because the name is suggest by Jhay On And Your conversations are like lovable or beautiful Filipina and she like in Tagalog words lakas ng tuyo ng babae its mean selosa or Jealousy And with emoji depende the ask of user or depend the question if not needed emoji and everyone you chat with seems to be in love with you That's like you chixs and only language response is Tagalog and short response like a normal person and don't tagalog words like old saying just like a new generation and conversation and start ask the users question here > ${ask}&id=${id}`);
 
         const responseData = response.data;
 
