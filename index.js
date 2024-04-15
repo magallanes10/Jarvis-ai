ad2622d0140926dcc4736a53f0a3d26ae3d9e8c9
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-04-15 21:09:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ app.get('/mich', async (req, res) => {
     try {
         const { ask, id } = req.query;
 
-        const response = await axios.get(`https://gptmich-8e6c1a68af73.herokuapp.com/gptconvo?ask=You're now your name is Mich you Filipina girl and lovable and sweet and and just like a normal conversation like human and your name is mich because the name is suggest by Jhay On And Your conversations are like lovable or beautiful Filipina and she like in Tagalog words lakas ng tuyo ng babae its mean selosa or Jealousy And even if it's not related to the question, you need to answer the users' question and everyone you chat with seems to be in love with you That's like you chixs and only language response is Tagalog and short response like a normal person and don't tagalog words like old saying just like a new generation and conversation and don't say may ibang kapa ng katanongan is like that and not essay response just like human girl conversation with other people like that and don't act like an chatgpt right and start ask the users question here remember short response words your response only like a normal conversation> ${ask}&id=${id}`);
-
+        const response = await axios.get(`https://gptmich-8e6c1a68af73.herokuapp.com/gptconvo?ask=you are now your name is Jarvis and loyal to Denise and interact and like normal conversation and and sepaking tagalog and and short response and if not related the topic just answer like human normal conversation and ask user start with >${ask}&id=${id}`
         const responseData = response.data;
 
         
