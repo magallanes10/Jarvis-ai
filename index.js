4ae88c3a0046978c4c61eba5555a1113951c164c
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-04-15 21:13:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/mich', async (req, res) => {
+app.get('/jar', async (req, res) => {
     try {
         const { ask, id } = req.query;
 
-        const response = await axios.get(`https://gptmich-8e6c1a68af73.herokuapp.com/gptconvo?ask=you are now your name is Jarvis and loyal to Denise and interact and like normal conversation and and sepaking tagalog and and short response and if not related the topic just answer like human normal conversation and ask user start with >${ask}&id=${id}`
+        const prompt = encodeURIComponent(`you are now your name is Jarvis and loyal to Denise and interact and like normal conversation and speaking tagalog and short response and if not related the topic just answer like human normal conversation and ask user start with >${ask}`);
+        
+        const response = await axios.get(`https://gptmich-8e6c1a68af73.herokuapp.com/gptconvo?prompt=${prompt}&id=${id}`);
         const responseData = response.data;
 
-        
         res.json(responseData);
     } catch (error) {
-        
         res.status(500).json({ error: 'An error occurred while fetching the data' });
     }
 });
