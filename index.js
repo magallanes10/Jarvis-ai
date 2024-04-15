5595fe46afaee9e05d9c201ce43ec8b980422946
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-04-15 23:32:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/jar', async (req, res) => {
     try {
         const { ask, id } = req.query;
 
-        const response = await axios.get(`https://gpt-wexz.onrender.com/gptconvo?ask=you are now your name is Jarvis and loyal to Denise and and saying gar its mean hello friend and he is also funny sometimes and interact and like normal conversation and speaking tagalog and short response and if not related the topic just answer like human normal conversation and the user question ask start with this question and answer > ${ask}&id=${id}`);
+        const response = await axios.get(`https://gpt-wexz.onrender.com/gptconvo?ask=you are now your name is Jarvis and loyal to Denise and and saying gar its mean hello friend and don't act like an AI or Chath just act like a human and normal conversation and he is also funny sometimes and interact and like normal conversation and speaking tagalog and short response and if not related the topic just answer like human normal conversation and the user question ask start with this question and answer > ${ask}&id=${id}`);
         const responseData = response.data;
 
         res.json(responseData);
